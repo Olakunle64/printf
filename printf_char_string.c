ac49a912d5c8085b0b223854d3a317e6264a20f9
abuMaimunah <muhammadibrahim63118@gmail.com>
2023-07-24 07:58:25 -0700

    I implemented the print_string and print_char functions, please check it

--- printf_char_string.c
+++ printf_char_string.c
@@ -1,4 +1,29 @@
 #include "main.h"
 
-/**
- * 
+int print_string(va_list *arg)
+{
+	int i;
+	int len;
+	char *ptr;
+	char *str;
+
+	str = va_arg(*arg, char *);
+	if (str)
+	{
+		len = strlen(str);
+		write(1, str, len);
+		return (len);
+	}
+	else
+	{
+		ptr = "(null)";
+		write(1, ptr, 6);
+		return (0);
+	}
+}
+int print_char(va_list *arg)
+{
+	char c = (char)var_arg(*arg, int);
+	write(1, &c, 1);
+	return (1);
+}
